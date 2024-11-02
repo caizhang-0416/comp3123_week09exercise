dd81dbfdf4ebafeb46590157d6f1517ad02619cc
Cai <caizhang0416@gmail.com>
2024-11-02 18:14:55 -0400

    Initial commit for Week 09 React exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [info, setInfo] = useState({
+    message: "Welcome to Fullstack Development! React JS Programming Week09 Lab exercise",
+    author: "Cai Zhang, ID: 101464891",
+    college: "George Brown College, Toronto"
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <p>{info.message}</p>
+        <p>Created by: {info.author}</p>
+        <p>Institution: {info.college}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
